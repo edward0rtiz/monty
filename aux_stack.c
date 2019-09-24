65632efc97064f078580f38599943596b61f77e6
edward0rtiz <931@holbertonschool.com>
2019-09-24 15:33:22 +0000

    [feature] add aux_stack.c

---
+++ aux_stack.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+int create_node_stack(stack_t **stack)
+{
+	stack_t *node_stack;
+
+	node_stack = malloc(sizeof(stack_t));
+	if (node_stack == NULL)
+/* has to be an error of malloc */
+		return (NULL);
+
+	node_stack->n = STACK;
+	node_stack->prev = NULL;
+	node_stack->next = NULL;
+
+	*stack = node_stack;
+
+	return  (EXIT_SUCCESS);
+}
+
+void free_node_stack(stack_t **stack)
+{
+	stack_t *temp_node = *stack;
+
+	while (*stack)
+	{
+		temp_node = (*stack)->next;
+		free(*stack);
+		*stack = temp_node;
+	}
+}
+
+int check_opcode(stack_t *stack)
+{
+	int ret_val = 2;
+
+	if (stack->n == STACK)
+		return (STACK);
+	else if(stack->n == QUEUE)
+		return (QUEUE);
+	return (ret_val)
+}
