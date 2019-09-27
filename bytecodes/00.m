3d26bab0b548f3fc1717e93d1d49d6a2c6b5deaf
edward0rtiz <931@holbertonschool.com>
2019-09-27 00:02:33 +0000

    merge

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,4 +1,18 @@
-push
-push 2
+pall
+push      -1
+push 1
+
+                  push       2
+
+         
 push 3
+push     4   Push 0 onto the stack Push 0 onto the stack
+push 5
+pall holberton
+push    6
+push 7
+         
+                 
+
+push
 pall
