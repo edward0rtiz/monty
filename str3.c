8127a22c19d4c5fec6eb2209615e550a8326d409
edward0rtiz <931@holbertonschool.com>
2019-09-27 15:13:29 +0000

    [feature] fix checker

--- str3.c
+++ str3.c
@@ -91,16 +91,12 @@ int _isdigit(void)
 
 	if (globalvar.token2[0] == '-' || globalvar.token2[0] == '+')
 		i++;
-	while (globalvar.token2)
+	while (globalvar.token2[i])
 	{
-		if ((globalvar.token2[i] >= '0' && (globalvar.token2[i + 1] >= '0'
-						    || globalvar.token2[i + 1] == '\0')) &&
-		    (globalvar.token2[i] <= '9' && (globalvar.token2[i + 1] <= '9'
-						    || globalvar.token2[i + 1] == '\0')))
-			return (1);
-		else
+
+		if (isdigit(globalvar.token2[i]) == 0)
 			return (0);
 		i++;
 	}
-	return (0);
+	return (1);
 }
